6f2f12d2361482c719a068b23e0359232beaf1d2
sileysa <sileysafaedatulnuraini984@gmail.com>
2024-10-29 09:51:55 +0700

    saya upload Triangle27 ke daspro-jobsheet8

--- Triangle27.java
+++ Triangle27.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
+    import java.util.Scanner;
 public class Triangle27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int N = 5;
-        for (int h = 1; h <= N; h++) {
-            if(h==1 || h==N);
+        for (int i = 1; i <= N; i++) {
+            if(i==1 || i==N);
 
-                for (int i = 1; i <= h; i++) {
+                for (int j = 1; j <= i; j++) {
                     System.out.print("*");
                 }
             System.out.println();
